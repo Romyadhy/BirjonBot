12a6827d8e033d62e6b46a17b61571e04ee8c40a
Romyadhy <romyadhy@student.undiksha.ac.id>
2025-01-06 21:49:53 +0800

    add gempa

--- main.js
+++ main.js
@@ -7,10 +7,25 @@ const options = {
 };
 const birjonbot =  new TelegramBot(token, options);
 
-birjonbot.on("message", (msg) => {
-    const chatId = msg.chat.id;
-    birjonbot.sendMessage(chatId, "Hello, I am Bijon Bot");
-});
+// birjonbot.on("message", (msg) => {
+//     const chatId = msg.chat.id;
+//     birjonbot.sendMessage(chatId, msg.text);
+// });
+
+// init variabel input
+const prefix = ".";
+const seyWelcome = new RegExp(`^${prefix}start$`);
+const gempa = new RegExp(`^${prefix}infogempa$`)
+
+birjonbot.onText(seyWelcome, (callback) => {
+    birjonbot.sendMessage(callback.chat.id, "Welcome to Birjon Bot");
+})
+
+birjonbot.onText(gempa, (callback) => {
+    birjonbot.sendMessage(callback.chat.id, "Ini wok beberapa info gempa terkini!!!");
+})
+
+
 
 // birjonbot.on("message", (callback) => console.log(callback));
 
